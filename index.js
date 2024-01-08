a5d060388d374de5928c31eeca31cf0d7d3c524f
chetansource <chetankumargopi149@gmail.com>
2024-01-08 20:55:03 +0530

    created my personal portfolio using html,css and js

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function downloadCV() {
+  var downloadLink =
+    "https://drive.google.com/file/d/10XAUAe3ujF1j_FWFLDI6Yujde0Fc6g0K/view?usp=drive_link";
+
+  // Trigger the download by setting the window location
+  window.location.href = downloadLink;
+}
