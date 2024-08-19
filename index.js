97c185e30c9b33bcb00aa0ac1a9b242c7857c55f
chetansource <chetankumargopi149@gmail.com>
2024-08-19 22:35:54 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function downloadCV() {
   var downloadLink =
-    "https://drive.google.com/file/d/10XAUAe3ujF1j_FWFLDI6Yujde0Fc6g0K/view?usp=drive_link";
+    "https://drive.google.com/file/d/1_UMCh3LhNdcyRz2GfBiUrcbFOjyVs4h_/view?usp=sharing";
 
   // Trigger the download by setting the window location
   window.location.href = downloadLink;
