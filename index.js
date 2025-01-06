f563a88437cc6d510d8f4b080821ce238d7c84e4
chetansource <chetan.141k@gmail.com>
2025-01-06 11:59:56 +0530

    added resume updated link

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function downloadCV() {
   var downloadLink =
-    "https://drive.google.com/file/d/1_UMCh3LhNdcyRz2GfBiUrcbFOjyVs4h_/view?usp=sharing";
+    "https://drive.google.com/file/d/1Vambat9kDsCsqWed8oIA4vN-C_OitIoX/view?usp=drive_link";
 
   // Trigger the download by setting the window location
   window.location.href = downloadLink;
